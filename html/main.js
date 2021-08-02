aa3a0c790eb2accdeb2b54095a26026fffca75f1
coolfire02 <coolfire02@vortexnetwork.me>
2021-08-02 16:41:25 +0800

    Added Typewriter animation, Assignment 2 Project complete

--- html/main.js
+++ html/main.js
@@ -7,11 +7,10 @@
 //Loading screen that occurs once per session
 //My Minecraft server's player count. (Via mcAPI)
 //Random message poping up in loading screen
+//Typewriter
 
 //Todo:
-// Focus on adding effects and animations
-// Turn on and off rain (Local storage)
-// 
+// Turn on and off rain (Local storage) 
 
 let cursor;
 let link;
@@ -19,13 +18,14 @@ let link;
 let loader;
 let content;
 
-
 //Loading screen functions start
 function runPageLoader()
 {
     setTimeout(() => {
         content.style.display = 'block'
         content.style.opacity = 1;
+
+
         fadeOut(loader);
         sessionStorage.setItem("loaded", "true");
         //console.log("loadedin func " + sessionStorage.getItem('loaded'));
@@ -47,13 +47,69 @@ function fadeOut(element) {
 
 function main() {
 
+    //Typewriter simulation start
+    let defaultTypeWriterText = $(".app-picture-text")[0].getElementsByTagName('p')[0].innerHTML;
+
+    let typewriterAnimation = function() {
+        var p = $(".app-picture-text")[0].getElementsByTagName('p')[0];
+        var displayStr = "";
+
+        var typeOut = setInterval(function() {
+
+            //Type out function, keep adding 1 char at a time till text length reached
+            if(displayStr.length < defaultTypeWriterText.length)
+                {
+                    displayStr += defaultTypeWriterText[displayStr.length];
+                    p.innerHTML = displayStr;
+                    if(displayStr.length < defaultTypeWriterText.length)
+                        p.innerHTML += "_";
+                }
+            
+            //Text length reached
+            else
+            {
+                let interval = 80;
+                let msBeforeDeleteText = 1300; //cooldown before delete text starts
+                let msCount = 0;
+
+                //Delete char by char until nothing left
+                var deleteText = setInterval(function() {
+                    msCount += interval;
+                    if(msCount >= msBeforeDeleteText)
+                    {
+                        if(displayStr.length > 0)
+                        {
+                            displayStr = displayStr.substring(0, displayStr.length-1);
+                            p.innerHTML = displayStr + "_";
+                        }
+                        else
+                        {
+                            clearInterval(deleteText);
+
+                            //Recalls function when animation is complete as this is suppose to continue endlessly
+                            typewriterAnimation();
+                        }
+                    }
+                }, interval);
+
+                clearInterval(typeOut);
+            }
+        }, 100);
+    }
+    typewriterAnimation();
+    //Type writer simulation end
+
+
     //Minecraft server API retrieval start
     let interval = 10000; //milisecs between each call
-    let ip = "hypixel.net" //not using my own server's playcount cause our IPtables block the pinger lol and im too lazy to get the pinger's IPs :clown:
     let refresher = function() {
-        $.getJSON("https://mcapi.us/server/status?ip=" + ip, 
+
+        //Using an online proxy OMFG this took way too long. Tried anyorigin but they're DOWN!, 20+ forums on CORS bypass and none gave me the answer jesus christ.
+        $.getJSON("https://api.allorigins.win/raw?url=" + encodeURIComponent('https://www.vortexnetwork.me/queries/server?id=1'), 
             function(json) {
                 let playerDiv = document.getElementById("players");
+                if(playerDiv == null)
+                    return;
                 if(playerDiv.getElementsByTagName('p').length == 0)
                 {
                     for(let i = 0; i < 2; ++i)
@@ -62,7 +118,7 @@ function main() {
                     }
                     playerDiv.getElementsByTagName('p')[0].innerHTML = "Server is Offline!";
                 }
-                if(json.status !== "success")
+                if(json.status_value !== 1)
                 {
                     //Server not found/offline
                     playerDiv.getElementsByTagName('p')[0].innerHTML = "Server is Offline!";
@@ -74,7 +130,7 @@ function main() {
                     //Server is online
            
                     playerDiv.getElementsByTagName('p')[0].innerHTML = "Server is Online";
-                    playerDiv.getElementsByTagName('p')[1].innerHTML = json.players.now + "/" + json.players.max + " Players on the server";
+                    playerDiv.getElementsByTagName('p')[1].innerHTML = json.player_count + "/" + json.player_count_max + " Players on the server";
                   
                     playerDiv.style.borderBottom = "solid 3px greenyellow";
                     
@@ -98,9 +154,9 @@ function main() {
     
     $.getJSON("https://raw.githubusercontent.com/Coolfire02/Y2S1_WEBDEV_A02/master/html/quotes.json", function(json)
     {
-        console.log(json);
-        console.log(json["quotes"]);
-        document.getElementById("random-load-text").innerHTML = json["quotes"][Math.floor(Math.random() * json["quotes"].length)];
+        let element = document.getElementById("random-load-text");
+        if(element != null)
+            element.innerHTML = json["quotes"][Math.floor(Math.random() * json["quotes"].length)];
     });
 
     if(sessionStorage.getItem("loaded") == null)
